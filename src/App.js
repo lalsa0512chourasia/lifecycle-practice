fc037e23b51f454038cefc10e5aa651ca6f6cd63
lalsa chourasia <lalsachourasia0512.com>
2022-04-27 12:13:29 +0530

    hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
-// import Compo1 from './hooks/Compo1';
+import Compo1 from './hooks/Compo1';
 import TestUseEffect from './hooks/useEffectHooks';
 
 function App() {
   const [showComp, setShowcompo] = useState(true)
   return (
     <div className="App">
-     {/* <Compo1/> */}
+     <Compo1/>
 
      {showComp ? <TestUseEffect/>: null}
      <button onClick={()=>setShowcompo(false)}>Component Will Unmount</button>
